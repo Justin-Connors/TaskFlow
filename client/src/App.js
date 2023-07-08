66392647dcc80de9e5290f2a99e8994ae0218646
justin connors <justinconnors@hotmail.ca>
2023-07-08 19:28:29 -0300

    components, pages, utils and app updated in client dir

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,67 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+import Nav from "./components/Nav/index";
+import Footer from "./components/Footer/index";
+import Home from "./pages/Home/index";
+import Signup from "./pages/Signup/index";
+import Login from "./pages/Login/index";
+import Auth from "./utils/auth";
+
+// graphql link
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// auth link to add token to request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// create apollo client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <div></div>;
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Nav />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home title="Metrics" />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
