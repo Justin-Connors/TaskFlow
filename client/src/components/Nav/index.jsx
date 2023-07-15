6ef9979dad4d80d44a705ebd416709029061635d
justin connors <justinconnors@hotmail.ca>
2023-07-15 20:09:36 -0300

    navbar started still WIP

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+  Icon,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import logo from "../../assets/TaskFlow-Logo.png";
+import LoginIcon from "@mui/icons-material/Login";
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Stack direction="row" spacing={2}>
+          <img
+            src={logo}
+            alt="TaskFlow"
+            style={{
+              // 10% of original size
+              width: "120px",
+              height: "41.5px",
+            }}
+          />
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="Home"
+          >
+            <HomeIcon />
+          </IconButton>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="Login"
+          >
+            <LoginIcon />
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    </AppBar>
   );
 };
 
