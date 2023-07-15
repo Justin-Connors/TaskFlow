1bec9401d689ebf764db678a0aea2c8be55b0cb8
justin connors <justinconnors@hotmail.ca>
2023-07-15 19:14:54 -0300

    package fix

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { MantineProvider } from "@mantine/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: "dark" }}
-    >
-      <App />
-    </MantineProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
