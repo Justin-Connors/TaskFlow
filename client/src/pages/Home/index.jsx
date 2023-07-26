99b9190b811c4b0d59fe82581790e137bdbce805
justin connors <justinconnors@hotmail.ca>
2023-07-26 16:05:16 -0300

    got rid of the background image for now

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -5,24 +5,25 @@ import background from "../../assets/homeImg.png";
 import styled from "@mui/material/styles/styled";
 import styles from "./Home.module.css";
 
-const Background = styled("div")({
-  position: "absolute",
-  width: "75%",
-  height: "75%",
-  backgroundImage: `url(${background})`,
-  backgroundPosition: "right bottom",
-  right: 15,
-  bottom: 100,
-  backgroundSize: "contain",
-  backgroundRepeat: "no-repeat",
-  zIndex: -1000,
-});
+// may get used at a later time
+// const Background = styled("div")({
+//   position: "absolute",
+//   width: "75%",
+//   height: "75%",
+//   backgroundImage: `url(${background})`,
+//   backgroundPosition: "right bottom",
+//   right: 15,
+//   bottom: 100,
+//   backgroundSize: "contain",
+//   backgroundRepeat: "no-repeat",
+//   zIndex: -1000,
+// });
 
 const Home = () => {
   return (
     <Grid container spacing={2} className={styles.sizeFix}>
       <CssBaseline />
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} sm={6} sx={{ margin: "auto", textAlign: "center" }}>
         <Typography
           variant="h3"
           component="div"
@@ -48,12 +49,12 @@ const Home = () => {
             mt: "1.5rem",
             textTransform: "none",
             width: "150px",
+            textAlign: "center",
           }}
         >
           Try TaskFlow
         </Button>
       </Grid>
-      <Background />
     </Grid>
   );
 };
